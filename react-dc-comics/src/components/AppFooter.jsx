4bd1e85643f7d21eb7cb605f8c7c0072b785ee11
andreapaccagnella94 <andreapacca94@live.it>
2025-09-27 12:25:49 +0200

    add the link for follow

--- react-dc-comics/src/components/AppFooter.jsx
+++ react-dc-comics/src/components/AppFooter.jsx
@@ -1,4 +1,5 @@
 import footerLogo from "../assets/img/dc-logo-bg.png"
+import fbLogo from "../assets/img/footer-facebook.png"
 export default function AppFooter() {
 
     const footer_menu = [
@@ -139,11 +140,56 @@ export default function AppFooter() {
             ]
         }
     ]
+
+    const followUs_menu = [
+        {
+            id: 1,
+            text: 'FOLLOW US',
+            list: [
+                {
+                    id: 1,
+                    text: 'facebook',
+                    img: fbLogo,
+                    link: '#',
+
+                },
+                {
+                    id: 2,
+                    text: 'twitter',
+                    img: fbLogo,
+                    link: '#',
+
+                },
+                {
+                    id: 3,
+                    text: 'MOVIES',
+                    img: fbLogo,
+                    link: '#',
+
+                },
+                {
+                    id: 4,
+                    text: 'TV',
+                    img: fbLogo,
+                    link: '#',
+
+                },
+                {
+                    id: 5,
+                    text: 'GAMES',
+                    img: fbLogo,
+                    link: '#',
+
+                }
+            ]
+        }
+    ];
+
     return (
         <footer>
 
-            <div className="container">
-                <div className="links">
+            <div className="links">
+                <div className="container">
 
                     <div className="row">
                         <div className="col">
@@ -177,48 +223,74 @@ export default function AppFooter() {
                     {/* <div className="col">
                         <h3>Pastificio</h3>
                         <ul>
-                            <li>
-                                <a href="">il Pastificio</a>
-                            </li>
-                            <li>
-                                <a href="">il Pastificio</a>
-                            </li>
-                            <li>
-                                <a href="">il Pastificio</a>
-                            </li>
-                            <li>
-                                <a href="">il Pastificio</a>
-                            </li>
-                            <li>
-                                <a href="">il Pastificio</a>
-                            </li>
+                        <li>
+                        <a href="">il Pastificio</a>
+                        </li>
+                        <li>
+                        <a href="">il Pastificio</a>
+                        </li>
+                        <li>
+                        <a href="">il Pastificio</a>
+                        </li>
+                        <li>
+                        <a href="">il Pastificio</a>
+                        </li>
+                        <li>
+                        <a href="">il Pastificio</a>
+                        </li>
                         </ul>
-                    </div>
-                    <div className="col">
+                        </div>
+                        <div className="col">
                         <h3>Products</h3>
                         <ul>
-                            <li>
-                                <a href="">Le Classiche</a>
-                            </li>
-                            <li>
-                                <a href="">il Pastificio</a>
-                            </li>
-                            <li>
-                                <a href="">il Pastificio</a>
-                            </li>
-                            <li>
-                                <a href="">il Pastificio</a>
-                            </li>
-                            <li>
-                                <a href="">il Pastificio</a>
-                            </li>
+                        <li>
+                        <a href="">Le Classiche</a>
+                        </li>
+                        <li>
+                        <a href="">il Pastificio</a>
+                        </li>
+                        <li>
+                        <a href="">il Pastificio</a>
+                        </li>
+                        <li>
+                        <a href="">il Pastificio</a>
+                        </li>
+                        <li>
+                        <a href="">il Pastificio</a>
+                        </li>
                         </ul>
-                    </div> */}
-                </div>
-                <div className="sing-up">
-
+                        </div> */}
                 </div>
             </div>
+            <div className="sing-up">
+                <div className="container">
+
+                    <div className="row">
+                        <div className="col">
+
+                        </div>
+                        <div className="col">
+                            {followUs_menu.map(item => (
+                                <div className="follow" key={item.id}>
+                                    <h3>{item.text}</h3>
+
+                                    {item.list.map(list_item => (
+
+
+                                        <a href={list_item.link} key={list_item.id}>
+                                            <img src={list_item.img} alt={list_item.text} />
+                                        </a>
+
+                                    ))
+                                    }
+
+                                </div>
+                            ))}
+                        </div>
+                    </div>
+                </div>
+            </div>
+
 
         </footer>
     )
